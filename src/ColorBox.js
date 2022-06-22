702f314a91646dc972941c0c53d6432bff249db3
leopantsulaia <levanpantsulaia@yahoo.com>
2022-06-22 06:04:35 +0400

    adding all

---
+++ src/ColorBox.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import './ColorBox.css';
+// import './Palette.css';
+
+class ColorBox extends Component {
+  render() {
+    const { name, background } = this.props;
+    return (
+      <CopyToClipboard text='hello'>
+        <div style={{ background }} className='colorBox'>
+          <div className='copyContainer'>
+            <div className='boxContent'>
+              <span>{name}</span>
+            </div>
+            <button className='copyButton'>Copy</button>
+          </div>
+          <span className='seeMore'>More</span>
+        </div>
+      </CopyToClipboard>
+    );
+  }
+}
+
+export default ColorBox;
