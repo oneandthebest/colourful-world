c263fa67d1a97b4bd29300a31654838e3c89410b
leopantsulaia <levanpantsulaia@yahoo.com>
2022-06-24 05:59:05 +0400

    added copied messages and copied animations

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.css';
-import './Palette.css';
+// import './Palette.css';
+// import { Link } from "react-router-dom";
+// import classNames from "classnames";
+// import { withStyles } from "@material-ui/styles";
+// import styles from "./styles/ColorBoxStyles";
 
 class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { copied: false };
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
+  changeCopyState() {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 1500);
+    });
+  }
   render() {
-    const { name, background } = this.props;
+    const { name, background, } = this.props;
+    const { copied } = this.state;
     return (
-      <CopyToClipboard text={background}>
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className='colorBox'>
+          <div style={{ background }} className={`copyOverlay ${copied && 'show'}`} />
+          <div className={`copyMsg  ${copied && 'show'}`}>
+            <h1>copied!</h1>
+            <p>{this.props.background}</p>
+          </div>
           <div className='copyContainer'>
             <div className='boxContent'>
               <span>{name}</span>
--- src/Palette.js
+++ src/Palette.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import './Palette.css';
 
-export default class Palette extends Component {
+class Palette extends Component {
   render() {
-    const colorBoxes = this.props.colors.map((color) => <ColorBox background={color.color} name={color.name}/>);
+    const colorBoxes = this.props.colors.map((color) => <ColorBox background={color.color} name={color.name} />);
     return (
       <div className='Palette'>
-        <div className='Palette-colors'>
-          {colorBoxes}
-        </div>
+        <div className='PaletteColors'>{colorBoxes}</div>
       </div>
     );
   }
 }
+export default Palette;
