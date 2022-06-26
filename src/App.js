e6c3b589b99662a1db814fcd269d4fc05bf31371
leopantsulaia <levanpantsulaia@yahoo.com>
2022-06-26 17:09:35 +0400

    slide visual changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
 function App() {
   return (
     <div>
-      <Palette {...seedColors[4]}/>
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
