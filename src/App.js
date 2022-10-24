002be76d6d06a8a44a7a0b2994230ac7672d8588
unknown <levanpantsulaia@yahoo.com>
2022-10-24 17:47:21 +0400

    function changed to class on App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
-function App() {
+// function App() {
+  class App extends Component {
+    render() {
   return (
+      // <Router>
+      //   <Route exact patch="/" render={() => <h1>Palette kai ramea</h1>} />
+      //   <Route exact patch="/palette/:id" render={() => <Palette/>} />
+      // </Router>
+
+
     <div>
       <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
+}
 }
 
 export default App;
